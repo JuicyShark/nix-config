0681cb751beaa29234051871562a8423e9a6054a
JuicyShark <maxwellb9879@gmail.com>
2024-05-01 16:41:47 +1000

    Fix autoformat issues

--- modules/home-manager/window-manager/wayland/hyprland/default.nix
+++ modules/home-manager/window-manager/wayland/hyprland/default.nix
@@ -20,21 +20,14 @@
 
     wayland.windowManager.hyprland = {
       enable = true;
-      # plugins = [ hy3.packages.x86_64-linux.hy3 ];
+      #plugins = [ hy3.packages.x86_64-linux.hy3 ];
       settings = {
-        monitor = [
-          ",highrr,0x0,1,bitdepth,10"
-          "HDMI-A-1,disable        ];
+        monitor = [ ",highrr,0x0,1,bitdepth,10" "HDMI-A-1,disable"];
         xwayland.force_zero_scaling = true;
-        input = {
-          kb_layout = "
-          us,us
-          ";
+        input = { kb_layout = "us,us";
             follow_mouse = 1;
             touchpad = {
-              natural_scroll = "
-          yes
-          ";
+              natural_scroll = "yes";
               };
             };
 
@@ -48,21 +41,9 @@
               resize_on_border = true;
               allow_tearing = true;
               no_focus_fallback = true;
-              "
-          col.inactive_border
-          " = "
-          rgba
-          (${config.colorScheme.palette.base03}ff)
-          ";
-            "
-          col.active_border
-          " = "
-          rgba
-          (${config.colorScheme.palette.base0D}ff)
-          ";
-            layout = "
-          dwindle
-          ";
+              "col.inactive_border" = "rgba(${config.colorScheme.palette.base03}ff)";
+              "col.active_border" = "rgba(${config.colorScheme.palette.base0D}ff)";
+              layout = "dwindle";
             };
 
             decoration = {
@@ -81,26 +62,10 @@
             };
 
             group = {
-              "
-          col.border_inactive
-          " = "
-          0
-          xff${config.colorScheme.palette.base03}";
-          "
-          col.border_active
-          " = "
-          0
-          xff${config.colorScheme.palette.base0B}";
-          "
-          col.border_locked_active
-          " = "
-          0
-          xff${config.colorScheme.palette.base0E}";
-          "
-          col.border_locked_inactive
-          " = "
-          0
-          xff${config.colorScheme.palette.base00}";
+              "col.border_inactive" = "rgba(${config.colorScheme.palette.base03}ff)";
+              "col.border_active" = "rgba(${config.colorScheme.palette.base0B}ff)";
+              "col.border_locked_active" = "rgba(${config.colorScheme.palette.base0E}ff)";
+              "col.border_locked_inactive" = "rgba(${config.colorScheme.palette.base00}ff)";
           groupbar = {
             enabled = false;
             font_family = config.font;
@@ -109,99 +74,53 @@
             priority = 3;
             render_titles = true;
             scrolling = false;
-            text_color = "
-          0
-          xff${config.colorScheme.palette.base05}";
-          "
-          col.inactive
-          " = "
-          0
-          xff${config.colorScheme.palette.base03}";
-          "
-          col.active
-          " = "
-          0
-          xff${config.colorScheme.palette.base0B}";
+            text_color = "rgba(${config.colorScheme.palette.base05}ff)";
+            "col.inactive" = "rgba(${config.colorScheme.palette.base03}ff)";
+            "col.active" = "rgba(${config.colorScheme.palette.base0B}ff)";
             };
           };
 
           dwindle = {
-            pseudotile = "
-          yes
-          ";
-            force_split = "
-          2
-          ";
-            preserve_split = "
-          yes
-          ";
+            pseudotile = "yes";
+            force_split = 2;
+            preserve_split = "yes";
             no_gaps_when_only = 1;
-            split_width_multiplier = "
-          1.08
-          ";
-            };
+            split_width_multiplier = "1.15";
+          };
 
-            master = {
-              new_is_master = "
-          false
-          ";
-            no_gaps_when_only = "
-          1
-          ";
-            orientation = "
-          center
-          ";
-            inherit_fullscreen = "
-          false
-          ";
-            always_center_master = "
-          true
-          ";
-            };
+          master = {
+            new_is_master = false;
+            no_gaps_when_only = 1;
+            orientation = "center";
+            inherit_fullscreen = false;
+            always_center_master = true;
+          };
 
-            gestures = {
-              workspace_swipe = "
-          on
-          ";
-            };
+          gestures = {
+            workspace_swipe = "on";
+          };
 
-            binds = {
-              focus_preferred_method = 1;
-              movefocus_cycles_fullscreen = false;
-            };
+          binds = {
+            focus_preferred_method = 1;
+            movefocus_cycles_fullscreen = false;
+          };
 
-            misc = {
-              enable_swallow = "
-          true
-          ";
+          misc = {
+            enable_swallow = "true";
             swallow_regex = "^(kitty)$";
-          new_window_takes_over_fullscreen = "
-          2
-          ";
+            new_window_takes_over_fullscreen = 2;
             disable_hyprland_logo = true;
-            vrr = "
-          0
-          ";
-                vfr = true;
-              };
-            };
+            vrr = 0;
+            vfr = true;
+          };
+        };
 
-            systemd = {
-              variables = [
-                "--all
-          "
+        systemd = {
+          variables = ["--all"
         ];
         extraCommands = [
-          "
-          systemctl --user
-          stop
-          graphical-session.target
-          "
-          "
-          systemctl --user
-          start
-          hyprland-session.target
-          "
+          "systemctl --user stop graphical-session.target"
+          "systemctl --user start hyprland-session.target"
         ];
       };
     };
