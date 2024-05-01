9b3cd387f2c771ad8aacdd9035fdbfacfd364f49
JuicyShark <maxwellb9879@gmail.com>
2024-05-02 09:50:14 +1000

    step 1. the eye patch has been found

--- modules/nixos/services/media.nix
+++ modules/nixos/services/media.nix
@@ -1,80 +1,94 @@
 {pkgs, lib, config, ...}:
-{	
+{
 	config = lib.mkIf config.homelab.enable {
-    users.groups.media = { }; 
+	users.users.media = { 
+	isSystemUser = true;
+	group = "media";
+	};
+
+    	users.groups.media = { };
 
 		systemd.tmpfiles.rules = [
-      "d /srv/media 0440 - media - - "
-      "d /torrent 0770 - deluge - -"
+      "d /srv/media 0770 media media"
+      "d /srv/media/downloading 0770 media media"
+      "d /torrent 0770 media media"
     ];
 
 		services = {
 			jellyfin = {
 				enable = true;
-				user = "juicy";
 				group = "media";
 				openFirewall = true;
       };
-			
+
 			deluge = {
 				enable = true;
         declarative = true;
-	user = "deluge";
-	group = "deluge";
-	openFirewall = true;
+	openFirewall = false;
+	group = "media";
 	authFile = pkgs.writeTextFile {
         	name = "deluge-auth";
-        	text = "${config.sops.secrets.deluge-secrets.path}";
+        	text = "localclient:password:10";
         };
         config = {
           copy_torrent_file = true;
           move_completed = true;
-					torrentfiles_location = "/torrent/files";
+	  group = "media";
+	  torrentfiles_location = "/torrent/files";
           download_location = "/torrent/downloading";
           move_completed_path = "/torrent/completed";
           dont_count_slow_torrents = true;
-          max_active_seeding = -1;
-          max_active_limit = -1;
-          max_active_downloading = 8;
-          max_connections_global = -1;
-					max_upload_speed = "500.0";
-					share_ratio_limit = "1.5";
-					allow_remote = true;
+          max_active_seeding = 6;
+          max_active_limit = 12;
+          max_active_downloading = 3;
+          max_connections_global = 250;
+	  max_upload_speed = 500;
+	  max_download_speed = 2000;
+	  share_ratio_limit = 2;
+	  allow_remote = true;
           daemon_port = 58846;
           random_port = false;
-					listen_ports = [6880 6880];
-				};
-				web = {
-					enable = true;
-					port = 9050;
-					openFirewall = true;
-				};
+	  listen_ports = [6881 6889];
+	  outgoing_interface = "enp3s0";
+	  enabled_plugins = [ "Label" ];
+
+	};
+		web = {
+			enable = true;
+			port = 9050;
+				openFirewall = true;
 			};
-			
+	};
+
 			prowlarr = {
 				enable = true;
 				openFirewall = true;
 			};
-			
+
 			sonarr = {
-				enable = true;
-				group = "media";
-				openFirewall =  true;	
-			};
-			
-			bazarr = {
-				enable = true;
-				group = "media";
-				openFirewall = true;
-			};
-			
-			radarr = {
 				enable = true;
 				group = "media";
 				openFirewall = true;
 			};
 
-		}; 
+			bazarr = {
+				enable = true;
+				group = "media";
+				openFirewall = true;
+			};
+
+			radarr = {
+				enable = true;
+				group = "media";
+				openFirewall = true;
+						
+			};
+			jackett = {
+				enable = true; 
+				group = "media";
+				openFirewall = true;
+			};
+		};
     sops.secrets = {
       deluge-secrets ={
         owner =  config.users.users.deluge.name;
