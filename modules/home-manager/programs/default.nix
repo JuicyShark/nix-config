64d7c437f790c4c8adc7fd93dbadb89849b4e0a1
JuicyShark <maxwellb9879@gmail.com>
2024-07-19 13:09:24 +1000

    add cargo

--- modules/home-manager/programs/default.nix
+++ modules/home-manager/programs/default.nix
@@ -1,21 +1,18 @@
-{pkgs, ... }:
-
-
-{
+{pkgs, ...}: {
   imports = [
     ./firefox.nix
     ./obs.nix
-	#./neo4j.nix
+    #./neo4j.nix
     #./cyberSec-packages.nix
     ./qutebrowser
     ./music.nix
     ./messaging.nix
-    #./kitty.nix replaced with more minimal foot
     ./foot.nix
   ];
-	home.packages = with pkgs; [
-		rustc
-		rustycli
-		zig
-	];
+  home.packages = with pkgs; [
+    rustc
+    cargo
+    rustycli
+    zig
+  ];
 }
